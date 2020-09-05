ef403c6c23f86c1235b1f901536f794878d6f577
Jing <jxw1070@student.bham.ac.uk>
2020-09-05 20:19:14 +0100

    first part of ch2 done

---
+++ ch2/sizeof.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    cout << "The size of int is " << sizeof(int) << "bytes" << endl;
+    cout << "The size of long is " << sizeof(long) << "bytes" << endl;
+    return 0;
+}
---
+++ ch2/ternary_operator.cpp
@@ -0,0 +1,9 @@
+// #include <iostream>
+// using namespace std;
+int main() {
+    int a = 3;
+    int b = 8;
+    int c = a>b?b-a:a-b;
+    // cout << c << '\n';
+    return 0;
+}
