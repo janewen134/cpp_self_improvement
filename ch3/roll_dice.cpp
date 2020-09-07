91feb2d8f2b5ad8967c717d929dafed65697ca92
Jing <jxw1070@student.bham.ac.uk>
2020-09-07 21:37:45 +0100

    Update roll_dice.cpp

--- ch3/roll_dice.cpp
+++ ch3/roll_dice.cpp
@@ -4,22 +4,23 @@ using namespace std;
  
 //投骰子、计算和数、输出和数
 int rollDice() {
-	int die1 = 1 + rand() % 6;
-	int die2 = 1 + rand() % 6;
-	int sum = die1 + die2;
-	cout << "player rolled " << die1 << " + " << die2 << " = " << sum << endl;
-	return sum;
+    int die1 = 1 + rand() % 6;
+    int die2 = 1 + rand() % 6;
+    int sum = die1 + die2;
+    cout << "player rolled " << die1 << "+" << die2 << " = " << sum << endl;
+    return sum;
 }
+
 enum GameStatus { WIN, LOSE, PLAYING };
  
 int main() {
-	int sum, myPoint;
-	GameStatus status;
- 
-	unsigned seed; 
-	cout<<"Please enter an unsigned integer: ";
-	cin >> seed;//输入随机数种子
-	srand(seed);//将种子传递给rand()
+    int sum, myPoint;
+    GameStatus status;
+
+    unsigned seed;
+    cout << "please enter an unsigned integer: ";
+    cin >> seed;
+    srand(seed); // save the seed for rand()
  
 	sum = rollDice(); //第一轮投骰子、计算和数
     switch (sum) {
@@ -27,11 +28,13 @@ int main() {
         case 11:
         status = WIN;
         break;
+
         case 2:   //和数为2、3或12则为负,状态为LOSE
         case 3: 
         case 12:
         status = LOSE;
         break;
+        
         default: /*其它情况,游戏尚无结果,状态为
                 PLAYING,记下点数,为下一轮做准备 */
         status = PLAYING;
