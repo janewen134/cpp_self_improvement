4f99745283bbb8d1eedba55316e58153bcd23519
Jing <jxw1070@student.bham.ac.uk>
2020-09-09 19:19:13 +0100

    ch5--data sharing and protection

---
+++ ch5/scope.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int i;				//全局变量，文件作用域
+int main() { 
+     i = 5;			//为全局变量i赋值
+     {				//子块1
+         int i;		//局部变量，局部作用域
+         i = 7;
+         cout << "i = " << i << endl;//输出7
+      }
+      cout << "i = " << i << endl;//输出5
+      return 0;
+}
